5e36d8c90771ea815d991b2caf1a79796d5a3d1a
Majkel14xd <mlidwa@wp.pl>
2023-12-10 02:50:08 +0100

    schema

--- Main/Strona_Django/rain_measurement_system/rain_measurement_system/settings.py
+++ Main/Strona_Django/rain_measurement_system/rain_measurement_system/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_extensions',
     "rain_measurement_system_app",
 ]
 
